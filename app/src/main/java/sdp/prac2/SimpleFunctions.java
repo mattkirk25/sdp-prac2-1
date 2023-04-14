4d4e41e9267a786ca9e5b4fe7428446de07703a7
Matthew Kirk <g20k6399@campus.ru.ac.za>
2023-04-14 23:20:02 +0200

    Fixed: 'SimpleFunctions.java' Task1 method, Updated: 'AppTest.java' to test Task2 method

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,10 +3,8 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {
-    }
-
-
+    public SimpleFunctions() {}
+    
     /** Function to return the sum of the elements of a at the indices specified by b
       * <BR><I>Pre-Condition</I>    Both of the lists are lists of integers<BR>
       * <BR><I>Post-Condition</I>   NONE<BR>
@@ -14,7 +12,7 @@ public class SimpleFunctions {
       * @param b    List of integers
       * @return     The sum of the elements of a at the indices specified by b
       */
-    public int Task1 (List<Integer> a List<integer> b) {
+    public int Task1 (List<Integer> a, List<Integer> b) {
         int sum = 0;
         for (int i = 0; i < b.size(); i++) {
             int indexToGet = b.get(i);
